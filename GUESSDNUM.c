c4218c63381f9ff7d8567879a4f9c144b2e176cc
Asher <124173983+99lash@users.noreply.github.com>
2023-10-31 18:32:56 +0800

    READY FOR FLOAT DATATYPE

--- GUESSDNUM.c
+++ GUESSDNUM.c
@@ -7,39 +7,41 @@
 //SUCA SUCA SUCA BYYYYLATT!!!!!!!!
 //HINDI NA HARDCODED YUNG 25%, MEANING DI PARA DI SUMABOG YUNG 25% CHANCE OF WINNING KAPAG INIBA YUNG SIZE/LIMITATION NG PAGHUHULAAN
 //EXAMPLE; THE DEFAULT IS GUESS BETWEEN 1-100 AND WE KNOW THAT 25% OF 100 IS 25.
-//NOW, IF THE GUESS NUMBER IS BETWEEN 1-120 THE VALUE 25% WILL BE CHANGE AUTOMATICALLY ACCORDINGLY TO ITS SIZE (120).
-//AND YUNG 25% NG 120 AY 30.!!!!!!!!!
+//NOW, IF THE GUESS NUMBER IS BETWEEN 1-120, THE VALUE 25% WILL BE CHANGE AUTOMATICALLY ACCORDINGLY TO ITS SIZE (120).
+//AND YUNG 25% OF 120 AY 30.!!!!!!!!!
 
 
 //REMOVE THE SINGLE-LINE COMMENTS FOR DEMO :))
 
 /*int rematt(int x)
+DISABLED MUNA!
 {
  int remainingAttempts = remainingAttempts - x;
 
 }
-DISABLED 
 */
-int main()
+void main()
 {
 
-int attempt = 0, userguess, secretnum, maxsecretnum, minsecretnum, numsize, chance, remainingAttempts = 100;
+int attempt = 0, userguess, secretnum, maxsecretnum, minsecretnum, numsize, chance;
 
 srand(time(0));
 
     printf("Enter your preferred limit size of guessing (e.g 50): ");
     scanf("%i", &numsize);
     secretnum = (rand() % numsize) +1;
-    printf("%i\n", secretnum); //REMOVE TO REVEAL THE SECRET NUMBER
+    printf("Secret Number : %i\n", secretnum); //REMOVE TO REVEAL THE SECRET NUMBER
     printf("Guess the secret number between 1-%i\n\n", numsize);
     
     while(attempt != numsize){
         printf("Enter your guess : ");
         scanf("%i", &userguess);
         attempt++;
-        printf("\nAttempt Counter : %i\n", attempt);
+        //printf("\nAttempt Counter : %i\n", attempt);
+        /*DISABLED, under construction!
         remainingAttempts = rematt(attempt);
-        //DISABLED printf("\t\t\t\tremaining attempts %i", remainingAttempts);
+         printf("\t\t\t\tremaining attempts %i", remainingAttempts);
+        */
         if(userguess < 1 || userguess > numsize){
             printf("Invalid! Please guess only between 1-%i\n\n", numsize);
         }
@@ -82,5 +84,5 @@ srand(time(0));
 
 
 
-return 0;
+getch();
 }
